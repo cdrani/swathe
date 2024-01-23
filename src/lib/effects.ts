f2f82649250f6728eb758a38bbb65efa810a6506
charles <charlesdrani@gmail.com>
2024-01-22 20:41:25 -0700

    feat: add effects libs (#1)

---
+++ src/lib/effects.ts
@@ -0,0 +1,25 @@
+const effectsList = [
+    'airbrush',
+    'chalkboard',
+    'collage',
+    'colored-chalkboard',
+    'colored-pencil',
+    'emboss',
+    'flannel',
+    'hallucination',
+    'infrared',
+    'low-ink-h',
+    'low-ink-v',
+    'mirror-h',
+    'mirror-v',
+    'mosaic',
+    'night-vision',
+    'none',
+    'pencil',
+    'photo-border',
+    'selective-color',
+    'warhol',
+    'watercolor'
+] as const
+
+export { effectsList }
