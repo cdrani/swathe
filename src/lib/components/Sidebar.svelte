cc1acbe2d60e8e0e63b5eab09e3d83bf6bbe70d5
cdrani <charlesdrani@gmail.com>
2024-01-23 22:23:32 -0700

    styles: update Sidebar ui positioning

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -18,19 +18,19 @@
 </script>
 
 <aside
-    class="flex-col space-between w-56 p-5 h-full text-gray-400 overflow-hidden overscroll-contain"
+    class="relative flex-col space-between place-content-center items-center w-64 h-full bg-gray-800 text-gray-400 overflow-hidden overscroll-contain"
 >
-    <div class="absolute top-0 left-0 px-7 pt-3 mb-2 bottom-2 bg-gray-800 w-56">
+    <div class="absolute top-0 flex-col w-full items-center left-5 pt-3 mb-2 bottom-2">
         <h2 class="text-2xl lowercase tracking-[16px]">Swathe</h2>
         <hr class="relative -left-5 w-52" />
     </div>
 
-    <section class="absolute left-0 top-12 bottom-0 bg-gray-800 p-5 w-56 py-5">
-        <ul class="flex flex-col h-full scroll-smooth overflow-auto overscroll-contain">
+    <section class="absolute left-0 top-12 bottom-0 bg-gray-800 w-full py-5">
+        <ul class="flex flex-col items-center h-full scroll-smooth overflow-auto overscroll-contain">
             {#each effectsList as effect (effect)}
                 <li
                     id={effect}
-                    class="mb-2 px-1 py-1 text-lg hover:rounded-md hover:font-bold hover:text-black hover:bg-white"
+                    class="mb-2 py-1 w-[80%] text-lg hover:rounded-md hover:font-bold hover:text-black hover:bg-white"
                     class:selection={$selection == effect}
                 >
                     <button class="w-full h-full" aria-label={effect} on:click={handleEffect}>
