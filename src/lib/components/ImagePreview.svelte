f31e8ed3361a861139c458a039b5e36e85d9b909
cdrani <charlesdrani@gmail.com>
2024-01-27 00:48:26 -0700

    components: include effect in ImagePreview props

--- src/lib/components/ImagePreview.svelte
+++ src/lib/components/ImagePreview.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
-    export let src: string
+    export let src: string = ''
+    export let effect: string = ''
+    export let dynamic: boolean = false
 </script>
 
-<div class="flex place-content-center w-full">
-    <img alt="preview" {src} class="w-full md:w-3/4 lg:w-5/6 bg-center bg-cover" />
-</div>
+<div
+    style="--src: url({src});"
+    class="flex w-full aspect-square image {dynamic ? effect : ''}"
+></div>
