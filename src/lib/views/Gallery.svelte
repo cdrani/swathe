0cabde509ebfa3cca2893a80bdff12a8ea5eb03e
cdrani <charlesdrani@gmail.com>
2024-01-29 14:37:37 -0700

    views: use grid system for Gallery

--- src/lib/views/Gallery.svelte
+++ src/lib/views/Gallery.svelte
@@ -5,8 +5,10 @@
     export let src: string
 </script>
 
-<div class="overflow-auto relative w-full h-full p-4 md:p-7 lg:p-10 xl:p-16 grid place-content-start grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 gap-4 md:gap-6 lg:gap-8">
+<div
+    class="overflow-auto relative w-full h-full p-4 md:p-7 lg:p-10 xl:p-16 grid place-content-start grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 gap-4 lg:gap-6"
+>
     {#each effectsList as effect (effect)}
-        <ImagePreview dynamic effect={effect} src={src} view="grid" />
+        <ImagePreview dynamic {effect} {src} view="grid" />
     {/each}
 </div>
