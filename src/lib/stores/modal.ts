5fb7aa90e5cc69a4f55f9cdbcde304bdb2f20e19
cdrani <charlesdrani@gmail.com>
2024-02-20 22:52:22 -0700

    styles: center Sidebar title

--- src/lib/stores/modal.ts
+++ src/lib/stores/modal.ts
@@ -5,10 +5,10 @@ import { getContext, setContext } from 'svelte'
 
 const STORE = 'modal'
 
-type Modal = { effect: Effect; visible: boolean }
+type Modal = { effect: Effect; visible: boolean; full: boolean }
 type Context = Writable<Modal>
 
-export const INIT_MODAL: Modal = { effect: 'none', visible: false }
+export const INIT_MODAL: Modal = { effect: 'none', visible: false, full: false }
 
 export function initModal() {
     const modal = writable(INIT_MODAL)
